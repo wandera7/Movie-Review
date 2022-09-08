d2eaa60159117b51efe948300b0904b28dd46739
wandera7 <julius.wandera@student.moringaschool.com>
2022-09-08 21:20:05 +0300

    PATCH req in Card component

---
+++ movie-review-client/src/components/Card.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Stack,IconButton,Tooltip } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+function MovieCard(){
+  const {id,name,desc,image_url,likes}=movie
+  const[words,setWords]=React.useState(true)
+  const [likeCount,setLikeCount]=React.useState(likes)
+  const [styleLike,setStyleLike]=React.useState(false)
+
+
+    function handleLike(){
+        setStyleLike(!styleLike)
+        setLikeCount((likes)=>likes+1)
+        fetch(`http://localhost:9292/movies/${id}`,{
+          method:'PATCH',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            likes:likeCount
+
+        })})
+        .then((res)=>res.json())
+        .then((data)=>onUpdate(data))
+      }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default MovieCard
